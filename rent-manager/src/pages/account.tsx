40fb5e442910f6b286c99f3bc52cb0be69debfd4
Vlad <vladpoienariu@gmail.com>
2025-02-28 15:13:06 +0200

    landlorad account tenant account first time mesage

--- rent-manager/src/pages/account.tsx
+++ rent-manager/src/pages/account.tsx
@@ -2,6 +2,7 @@ import Button from "../components/button";
 import Section from "../components/section";
 
 import leaf from "../assets/leaf.svg";
+import { useRef } from "react";
 
 export default function Account() {
 
@@ -9,23 +10,45 @@ export default function Account() {
       <Section>
          <img src={leaf} alt="decor leaft" className="background-decor leaf-1" />
          <img src={leaf} alt="decor leaft" className="background-decor leaf-2" />
+
          <div className="account d-height">
-            {/* <FirstRun /> */}
-            <UserAcccount />
+            <div className="user-account d-height">
+               <div className="user-details">
+                  <h2>Welcome, Vlad!</h2>
+                  <p>Email: vladpoienariu@gmail.com</p>
+                  <p>Tel: +40723382048</p>
+               </div>
+               {/* <FirstRun /> */}
+
+               <LandlordAccount />
+               {/* <TenantAcccount /> */}
+            </div>
          </div>
       </Section>
    )
 }
 
-function UserAcccount() {
+function TenantAcccount() {
    return (
-      <div className="user-account d-height">
-         <div className="user-details">
-            <h2>Welcome, Vlad!</h2>
-            <p>Email: vladpoienariu@gmail.com</p>
-            <p>Tel: +40723382048</p>
-         </div>
-         <div className="rents-wrapper">
+      <div className="rents-wrapper">
+      <h2>My rent:</h2>
+
+      <div className="rents">
+         <Rent />
+         <Rent />
+         <Rent />
+         <Rent />
+      </div>
+   </div>
+   )
+}
+
+function LandlordAccount() {
+   const imagePreview = useRef<HTMLImageElement>();
+
+   return (
+      <>
+          <div className="rents-wrapper">
             <h2>My rent:</h2>
 
             <div className="rents">
@@ -34,13 +57,56 @@ function UserAcccount() {
                <Rent />
                <Rent />
             </div>
+
+            <div className="rents-wrapper new-rent-wrapper">
+            <h2>New rent:</h2>
+
+            <div className="new-rent">
+               <form action="" className="add-rent-form">
+                  <div className="form-image">
+                     <img
+                        ref={imagePreview}
+                        className="preview-image"
+                        src="" />
+                     <input
+                        onChange={(e) => {
+                           const file = e.target.files[0];
+
+                           if (file) {
+                              const reader = new FileReader();
+
+                              reader.onload = function (e) {
+                                 imagePreview.current.src = e.target?.result;
+                              };
+
+                              reader.readAsDataURL(file);
+                           }
+                        }}
+                        type="file"
+                        name="image"
+                        id="image"
+                        className="input-field"
+                        accept="image/*" />
+                  </div>
+                  <div className="input">
+                     <input type="text" placeholder="Rent name" className="input-field" />
+                  </div>
+                  <Button
+                     type="PRIMARY"
+                     cb={() => {
+
+                     }}
+                  >Create rent</Button>
+               </form>
+            </div>
          </div>
-      </div>
+         </div>
+      </>
    )
 }
 
 function Rent() {
-   return(
+   return (
       <div className="rent">
          <div className="rent-image-wrapper">
             <img src="https://images.unsplash.com/photo-1523217582562-09d0def993a6?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="rent-image" alt="" />
@@ -50,16 +116,16 @@ function Rent() {
             <p>#1234123</p>
          </div>
          <div className="buttons">
-            <Button 
-            type="DANGER" 
-            cb={() => {
+            <Button
+               type="DANGER"
+               cb={() => {
 
-            }}>Leave</Button>
-             <Button 
-            type="PRIMARY" 
-            cb={() => {
+               }}>Leave</Button>
+            <Button
+               type="PRIMARY"
+               cb={() => {
 
-            }}>Dashboard</Button>
+               }}>Dashboard</Button>
          </div>
       </div>
    )
