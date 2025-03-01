839edf507f5b6dc3c0780392f9861a9b74cf5bdc
Vlad <vladpoienariu@gmail.com>
2025-03-01 17:40:43 +0200

    manage and dashboard page resposnive

--- rent-manager/src/pages/Rent/dashboard.tsx
+++ rent-manager/src/pages/Rent/dashboard.tsx
@@ -1,12 +1,107 @@
+import { formatTime } from "../../assets/functions/functions";
+import Button from "../../components/button";
 import Section from "../../components/section";
 
 export default function Dashboard() {
 
+
+
+   return (
+      <Section>
+         <div className="rent-dashboard d-height">
+            <header>
+               <h2 className="rent-name">Floreasca 53</h2>
+               <p>{formatTime().now()}</p>
+            </header>
+
+            <div className="usages">
+               <Utilities />
+               <TotalCalculator />
+            </div>
+
+            <div className="buttons">
+                  <Button
+                  type="SECONDARY"
+                  cb={() => {}}
+                  >Contact Landlord</Button>
+
+                  <Button
+                  type="PRIMARY"
+                  cb={() => {}}
+                  >Send readings</Button>
+            </div>
+         </div>
+      </Section>
+   )
+}
+
+function Utilities() {
+   return (
+      <div className="utilities">
+         <h2>Utilities</h2>
+         <div className="utilities-grid">
+            <Utility />
+            <Utility />
+            <Utility />
+            <Utility />
+            <Utility />
+         </div>
+      </div>
+   )
+}
+
+function Utility() {
+
+   return (
+      <div className="utility">
+         <h2>Water use</h2>
+         <div className="circle">
+            <div className="usage">
+               <h1 className="usage-value">4</h1>
+               <p className="units">m3</p>
+            </div>
+            <p className="aproximately-cost">~ 10.55Eur</p>
+            <p className="index">Last index: 2313</p>
+         </div>
+      </div>
+   )
+}
+
+function TotalCalculator() {
+
    return(
-   <Section>
-           <div className="rent-dashboard d-height">
-            
-           </div>
-    </Section>
+      <div className="total-calculator">
+         <h2>Total</h2>
+
+         <div className="wrapper">
+            <div className="progress-bar">
+               <Progress name={"Ethernet"} price={23} type="ethernet" value={5} />
+               <Progress name={"Gas"} price={23} type="gas" value={20} />
+               <Progress name={"Water"} price={23} type="water" value={20} />
+               <Progress name={"Rent"} price={23} type="rent" value={55} />
+            </div>
+            <h3 className="total">Total: 333 Euro</h3>
+         </div>
+      </div>
+   )
+}
+
+function Progress(props: {
+   type: string,
+   value: number,
+   price: number,
+   name: string,
+}) {
+
+   return(
+      <div 
+      style={{
+         width: `${props.value}%`,
+      }}
+   
+      className={`progress ${props.type}`}>
+         <p>{props.name}</p>
+         <span>{props.name} {props.price} Euro</span>
+      </div>
    )
 }
--- rent-manager/src/pages/Rent/manage.tsx
+++ rent-manager/src/pages/Rent/manage.tsx
@@ -1,10 +1,64 @@
+import { formatTime } from "../../assets/functions/functions";
+import Button from "../../components/button";
 import Section from "../../components/section";
 
 export default function Manage() {
 
-   return(
-    <Section>
-        <div className="rent-manage d-height"></div>
-    </Section>
+
+
+   return (
+      <Section>
+         <div className="rent-dashboard d-height">
+            <header>
+               <h2 className="rent-name">Floreasca 53</h2>
+               <p>{formatTime().now()}</p>
+            </header>
+
+            <div className="usages">
+               <Utilities />
+        
+            </div>
+
+            <div className="buttons">
+                  <Button
+                  type="SECONDARY"
+                  cb={() => {}}
+                  >Delete rent</Button>
+
+                  <Button
+                  type="PRIMARY"
+                  cb={() => {}}
+                  >Save readings</Button>
+            </div>
+         </div>
+      </Section>
    )
 }
+
+function Utilities() {
+   return (
+      <div className="utilities">
+         <h2>Utilities</h2>
+         <div className="utilities-grid manage-utilty">
+            <Utility />
+            <Utility />
+            <Utility />
+            <Utility />
+            <Utility />
+         </div>
+      </div>
+   )
+}
+
+function Utility() {
+
+   return (
+      <div className="utility">
+         <h2>Water index: </h2>
+         <div className="input">
+            <input type="text" placeholder="Water index" className="input-field" />
+         </div>
+      </div>
+   )
+}
+
